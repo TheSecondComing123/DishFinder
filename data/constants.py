7f405796e90e81e4ee93a724f1667c99a3cb9e90
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2025-08-31 10:50:31 -0400

    Inital commit

---
+++ data/constants.py
@@ -0,0 +1,12 @@
+# constants.py
+
+# User dict keys
+USER_ID = 'id'
+USERNAME = 'username'
+PASSWORD = 'password'
+
+# Dish dict keys
+DISH_ID = 'id'
+DISH_NAME = 'name'
+DISH_TAGS = 'tags'
+DISH_REVIEWS = 'reviews'
